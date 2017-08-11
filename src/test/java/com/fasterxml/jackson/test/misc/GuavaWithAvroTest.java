d17d16e6b48e4551e7d85b4505bec61a46072910
Tatu Saloranta <tatu.saloranta@iki.fi>
2017-08-10 20:31:58 -0700

    Add an Avro/Guava test

---
+++ src/test/java/com/fasterxml/jackson/test/misc/GuavaWithAvroTest.java
@@ -0,0 +1,40 @@
+package com.fasterxml.jackson.test.misc;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.avro.AvroMapper;
+import com.fasterxml.jackson.dataformat.avro.AvroSchema;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+
+import com.fasterxml.jackson.test.BaseTest;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+
+public class GuavaWithAvroTest extends BaseTest
+{
+    static class MultimapHolder {
+        public Multimap<String, String> map = ArrayListMultimap.create();
+    }
+
+    public void testGenerateAvroForMultimap() throws Exception
+    {
+        final AvroMapper avroMapper = new AvroMapper();
+        avroMapper.registerModule(new GuavaModule());
+        final AvroSchema schema = avroMapper.schemaFor(MultimapHolder.class);
+        
+        ObjectWriter avroWriter = avroMapper.writer(schema);
+        MultimapHolder input = new MultimapHolder();
+        input.map.put("a", "b");
+        input.map.put("a", "x");
+        input.map.put("b", "y");
+        final byte[] avro = avroWriter.writeValueAsBytes(input);        
+
+        MultimapHolder output = avroMapper.readerFor(MultimapHolder.class)
+                .with(schema)
+                .readValue(avro);
+        assertEquals(3, output.map.size());
+        assertEquals(Arrays.asList("b", "x"), output.map.get("a"));
+        assertEquals(Arrays.asList("y"), output.map.get("b"));
+    }
+}
