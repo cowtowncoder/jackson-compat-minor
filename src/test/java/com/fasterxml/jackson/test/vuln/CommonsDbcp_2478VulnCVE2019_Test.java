66c6f14dc35393344073f49402bce84ba1dfc2cf
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-09-28 20:11:12 -0700

    improve test wrt 2478

--- src/test/java/com/fasterxml/jackson/test/vuln/CommonsDbcp_2478VulnCVE2019_Test.java
+++ src/test/java/com/fasterxml/jackson/test/vuln/CommonsDbcp_2478VulnCVE2019_Test.java
@@ -1,12 +1,22 @@
 package com.fasterxml.jackson.test.vuln;
 
+import org.apache.commons.dbcp.datasources.PerUserPoolDataSource;
 import org.apache.commons.dbcp.datasources.SharedPoolDataSource;
 import com.google.common.collect.ImmutableMap;
 import com.p6spy.engine.spy.P6DataSource;
 
 public class CommonsDbcp_2478VulnCVE2019_Test extends VulnTestBase
 {
-    public void testDBCPDataSource() throws Exception
+    public void testDBCPDataSource1() throws Exception
+    {
+        _testIllegalType(PerUserPoolDataSource.class,
+                ImmutableMap.of("dataSourceName",
+                        "rmi://127.0.0.1:1389/Exploit"
+                )
+        );
+    }
+
+    public void testDBCPDataSource2() throws Exception
     {
         _testIllegalType(SharedPoolDataSource.class,
                 ImmutableMap.of("dataSourceName",
