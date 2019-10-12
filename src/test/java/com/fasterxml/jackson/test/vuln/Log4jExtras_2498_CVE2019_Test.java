de66416cb551e788a8f85b4fdff5dee21e3ed9ee
Tatu Saloranta <tatu.saloranta@iki.fi>
2019-10-12 10:55:59 -0700

    Add a test for db issue 2498

---
+++ src/test/java/com/fasterxml/jackson/test/vuln/Log4jExtras_2498_CVE2019_Test.java
@@ -0,0 +1,27 @@
+package com.fasterxml.jackson.test.vuln;
+
+import org.apache.log4j.receivers.db.DriverManagerConnectionSource;
+import org.apache.log4j.receivers.db.JNDIConnectionSource;
+
+import com.google.common.collect.ImmutableMap;
+
+public class Log4jExtras_2498_CVE2019_Test extends VulnTestBase
+{
+    public void testManagedConnectionSource() throws Exception
+    {
+        _testIllegalType(DriverManagerConnectionSource.class,
+                ImmutableMap.of("url",
+                        "jdbc:h2:tcp://127.0.0.1:1389/~/test"
+                )
+        );
+    }
+
+    public void testJNDIConnectionSource() throws Exception
+    {
+        _testIllegalType(JNDIConnectionSource.class,
+                ImmutableMap.of("jndiLocation",
+                        "ldap://localhost:1389/Exploit"
+                )
+        );
+    }
+}
