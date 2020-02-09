5ea48c1c6df29e94d934321637305486a067ff76
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-02-09 15:14:12 -0800

    add a test wrt databind issue 2620

--- src/test/java/com/fasterxml/jackson/test/vuln/Xalan_2469_CVE_Test.java
+++ src/test/java/com/fasterxml/jackson/test/vuln/Xalan_2469_CVE_Test.java
@@ -7,7 +7,7 @@ import org.apache.xalan.lib.sql.JNDIConnectionPool;
 // [databind#2469] with xalan2
 public class Xalan_2469_CVE_Test extends VulnTestBase
 {
-    public void testCommonsConfig1() throws Exception
+    public void testXalan2Vuln() throws Exception
     {
         _testIllegalType(JNDIConnectionPool.class,
                 Collections.singletonMap("jndiPath", "ldap://127.0.0.1:1389/Exploit"));
