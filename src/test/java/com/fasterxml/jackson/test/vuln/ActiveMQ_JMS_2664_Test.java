9117ba7ef20d7438b45a018dfa83ccd4ea046398
Tatu Saloranta <tatu.saloranta@iki.fi>
2020-03-25 13:12:19 -0700

    add test for db/2666

--- src/test/java/com/fasterxml/jackson/test/vuln/ActiveMQ_JMS_2664_Test.java
+++ src/test/java/com/fasterxml/jackson/test/vuln/ActiveMQ_JMS_2664_Test.java
@@ -8,8 +8,6 @@ public class ActiveMQ_JMS_2664_Test
     public void testRmiProvider() throws Exception
     {
         _testIllegalType(org.apache.activemq.jms.pool.XaPooledConnectionFactory.class,
-        // earlier possible different package:
-    //    _testIllegalType(org.aoju.bus.proxy.provider.remoting.RmiProvider.class,
                 Collections.singletonMap("tmJndiName", "ldap://localhost:1389/MyCalc")
                 // note: may need other properties too (tmFromJndi) for real repro
                 );
